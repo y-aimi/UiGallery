ca91fb94be6750598646bba378cb0dd3b4e21f24
yoshikiaimi <yoshiki0885@gmail.com>
2024-11-07 17:44:48 +0900

    iconのパスを修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,22 +62,22 @@ export default defineNuxtConfig({
       background_color: '#ffffff',
       icons: [
         {
-          src: 'icons-192x192.png',
+          src: '/icon-192x192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: 'icons-256x256.png',
+          src: '/icon-256x256.png',
           sizes: '256x256',
           type: 'image/png',
         },
         {
-          src: 'icons-384x384.png',
+          src: '/icon-384x384.png',
           sizes: '384x384',
           type: 'image/png',
         },
         {
-          src: 'icons-512x512.png',
+          src: '/icon-512x512.png',
           sizes: '512x512',
           type: 'image/png',
         },
